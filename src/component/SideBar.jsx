4014f6b444dced2b225a290d63d67380d5738244
Aditya Bayu Prabowo <Aditya07bayu@gmail.com>
2024-08-26 16:58:19 +0700

    Feat: SideBar

--- src/component/SideBar.jsx
+++ src/component/SideBar.jsx
@@ -1,25 +1,108 @@
-import { Sidebar } from "flowbite-react"
-import { BsPerson } from "react-icons/bs"
-import { HiChartPie } from "react-icons/hi2"
+import { Sidebar } from "flowbite-react";
+import {
+  BsPerson,
+  BsBook,
+  BsGear,
+  BsBoxArrowInRight,
+  BsEye,
+  BsPencil,
+  BsUnlock,
+  BsPalette,
+} from "react-icons/bs";
+import { HiChartPie } from "react-icons/hi2";
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../store/slices/authSlice";
+import { TbAlignBoxLeftStretch } from "react-icons/tb";
 
-const SideBar=()=>{
-    return(
-        <div>
-            <Sidebar aria-label="Sidebar with content">
-                <Sidebar.Items>
-                    <Sidebar.ItemGroup>
-                        <Sidebar.Item href="#" icon={HiChartPie}>
-                            Dashboard
-                        </Sidebar.Item>
-                        <Sidebar.Item href="#" icon={BsPerson}>
-                            Team
-                        </Sidebar.Item>
-                        
-                    </Sidebar.ItemGroup>
-                </Sidebar.Items>
-            </Sidebar>
-        </div>
-    )
-}
+const SideBar = () => {
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
 
-export default SideBar
+  return (
+    <div className="h-screen w-64 bg-slate-900">
+      <Sidebar aria-label="Sidebar with content">
+        <Sidebar.Items>
+          <Sidebar.ItemGroup>
+            <Sidebar.Item href="#" icon={HiChartPie}>
+              Dashboard
+            </Sidebar.Item>
+            <Sidebar.Collapse
+              icon={BsPerson}
+              label="Orphanages Data"
+              initialState="collapsed"
+            >
+              <Sidebar.Item href="#" icon={BsBoxArrowInRight}>
+                Create Orphanage
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={TbAlignBoxLeftStretch}>
+                Edit Orphanage
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={BsEye}>
+                View Orphanage
+              </Sidebar.Item>
+            </Sidebar.Collapse>
+            <Sidebar.Collapse
+              icon={BsBook}
+              label="Book Data"
+              initialState="collapsed"
+            >
+              <Sidebar.Item href="#" icon={BsBoxArrowInRight}>
+                Create Book
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={TbAlignBoxLeftStretch}>
+                Edit Book
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={BsPencil}>
+                Request Book
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={BsEye}>
+                View Book
+              </Sidebar.Item>
+            </Sidebar.Collapse>
+            <Sidebar.Collapse
+              icon={BsPerson}
+              label="Donatur Data"
+              initialState="collapsed"
+            >
+              <Sidebar.Item href="#" icon={BsBoxArrowInRight}>
+                Create Donatur
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={TbAlignBoxLeftStretch}>
+                Edit Donatur
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={BsEye}>
+                View Donatur Data
+              </Sidebar.Item>
+            </Sidebar.Collapse>
+            <Sidebar.Collapse
+              icon={BsGear}
+              label="Setting"
+              initialState="collapsed"
+            >
+              <Sidebar.Item href="#" icon={BsUnlock}>
+                Password Setting
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={BsPalette}>
+                Thema Setting
+              </Sidebar.Item>
+            </Sidebar.Collapse>
+          </Sidebar.ItemGroup>
+        </Sidebar.Items>
+        {isAuthenticated && (
+          <Sidebar.Item>
+            <div className="fixed bottom-0 left-0 w-64 p-4">
+              <button
+                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                onClick={() => dispatch(logout())}
+              >
+                Logout
+              </button>
+            </div>
+          </Sidebar.Item>
+        )}
+      </Sidebar>
+    </div>
+  );
+};
+
+export default SideBar;
--- src/pages/admin/HomePage.jsx
+++ src/pages/admin/HomePage.jsx
@@ -3,8 +3,13 @@ import SideBar from "../../component/SideBar"
 const HomePage = () => {
 
     return (
-        <div>
-            <SideBar/>
+        <div className="flex h-screen">
+            <div className="w-64">
+                <SideBar/>
+            </div>
+            <div className="flex-1 p-6">
+                <h1 className="text-3xl font-bold">Dashboard</h1>
+            </div>
         </div>
     )
 }
